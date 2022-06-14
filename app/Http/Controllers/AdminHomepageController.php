7661ab2b2ad7d01ee53f16442baad42c511b3e57
nikulaswalor <no@walor.io>
2022-06-14 19:30:26 +0200

    homepage invoke logic removed

--- app/Http/Controllers/AdminHomepageController.php
+++ app/Http/Controllers/AdminHomepageController.php
@@ -10,25 +10,6 @@ class AdminHomepageController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $ditoNumbers;
-
-        if($request->query('filter') == 'uninteresting') {
-            $ditoNumbers = DitoNumber::where('is_not_interesting', 1);
-        } else if($request->query('filter') == 'completed') {
-            $ditoNumbers = DitoNumber::where('is_selection_completed', 1);
-        } else {
-            $ditoNumbers = DitoNumber::where('is_selection_completed', 0)->where('is_not_interesting', 0);
-        }
-
-        if($request->input('search') ) {
-            $ditoNumber = $ditoNumbers
-                ->where(function($query) use($request) {
-                     $query->where('producer', 'like', '%' . $request->input('search') . '%');
-                     $query->orWhere('brand', 'like', '%' . $request->input('search') . '%');
-                });
-        }
-
-        $ditoNumbers = $ditoNumbers->paginate(50)->withQueryString();
-        return view('admin.index', compact('ditoNumbers'));
+        return 'homepage';
     }
 }
