ea56d00b1a43d0aa6a261191f57d48418f18c695
nikulaswalor <no@walor.io>
2022-06-15 21:11:13 +0200

    remove duplicates command

---
+++ app/Console/Commands/RemoveDuplicateGermanDismantlerCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\GermanDismantler;
+use App\Models\DitoNumberGermanDismantler;
+
+class RemoveDuplicateGermanDismantlerCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'german-dismantler-duplicates:remove';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $dismantlers = GermanDismantler::all();
+
+        foreach($dismantlers as $dismantler) {
+            $uniqueDismantler = GermanDismantler::where('hsn', '0005')
+                ->where('tsn', '438');
+            if($uniqueDismantler->get()->count() > 1) {
+                DitoNumberGermanDismantler::where('german_dismantler_id', $uniqueDismantler->first()->id)
+                    ->delete();
+                $uniqueDismantler->first()->delete();
+            }
+        }
+    }
+}
