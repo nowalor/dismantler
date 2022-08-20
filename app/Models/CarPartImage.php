ee16a57809a9a8b5e50db55652019fc6e89d28a4
nowalor <no@walor.io>
2022-08-20 13:37:11 +0200

    reelationships

--- app/Models/CarPartImage.php
+++ app/Models/CarPartImage.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CarPartImage extends Model
 {
     use HasFactory;
+
+    public function CarPart(): BelongsTo
+    {
+        return $this->belongsTo(CarPart::class);
+    }
 }
--- app/Models/CarPartType.php
+++ app/Models/CarPartType.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CarPartType extends Model
 {
     use HasFactory;
 
     protected $fillable = ['id', 'name', 'code'];
+
+    public function carPart(): HasMany
+    {
+        return $this->hasMany(CarPart::class);
+    }
 }
