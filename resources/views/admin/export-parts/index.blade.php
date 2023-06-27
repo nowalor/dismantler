e8e5342e2d05d80276272204ee27cc3cbb0543dc
nowalor <no@walor.io>
2023-06-27 15:55:16 +0200

    progress showing images

--- resources/views/admin/export-parts/index.blade.php
+++ resources/views/admin/export-parts/index.blade.php
@@ -1,1 +1,22 @@
-hello
+@extends('app')
+@section('title', 'Admin - Fenix Parrts')
+@section('content')
+    <div class="container">
+        <div class="d-flex flex-wrap">
+            @foreach($carParts as $carPart)
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-header">
+                            Hello
+                        </div>
+                        <div class="card-body">
+                            <img class="card-img-bottom"
+                                 src="{{ count($carPart->carPartImages) ? asset("storage/img/car-part/{$carPart->id}/{$carPart->carPartImages[0]->image_name}") : '' }}"
+                                 alt="">
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
