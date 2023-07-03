594aac39637f1ea6eda5a0c769671f3070f54af6
nowalor <no@walor.io>
2023-07-03 14:18:19 +0200

    feat: displaying more fields

--- resources/views/admin/export-parts/index.blade.php
+++ resources/views/admin/export-parts/index.blade.php
@@ -4,17 +4,19 @@
     <div class="container">
         <div class="d-flex flex-wrap">
             @foreach($carParts as $carPart)
-                <div class="col-6">
+                <div class="col-6 p-4">
                     <div class="card">
                         <div class="card-header">
                             {{ $carPart->name }}
                         </div>
                         <div class="card-body">
-                            <p> <span class="bold">Article number: </span> {{ $carPart->article_nr }}</p>
-                            <p> <span class="bold">Original number: </span> {{ $carPart->original_number }}</p>
-                            <p> <span class="bold">Price(EUR): </span> €{{ $carPart->price }}</p>
-                            <p> <span class="bold">Engine code: </span> {{ $carPart->engine_code }}</p>
-                            <p> <span class="bold">Engine type: </span> {{ $carPart->engine_type }}</p>
+                            <p> <span class="fw-bold">Article number: </span> {{ $carPart->article_nr }}</p>
+                            <p> <span class="fw-bold">Original number: </span> {{ $carPart->original_number }}</p>
+                            <p> <span class="fw-bold">Price(EUR): </span> €{{ $carPart->price }}</p>
+                            <p> <span class="fw-bold">Engine code: </span> {{ $carPart->engine_code }}</p>
+                            <p> <span class="fw-bold">Engine type: </span> {{ $carPart->engine_type }}</p>
+                            <p> <span class="fw-bold">Fuel: </span> {{ $carPart->fuel }}</p>
+                            <p> <span class="fw-bold">Gearbox: </span> {{ $carPart->gearbox }}</p>
                             <img class="card-img-bottom"
                                  src="{{ count($carPart->carPartImages) ? asset("storage/img/car-part/{$carPart->id}/{$carPart->carPartImages[0]->image_name}") : '' }}"
                                  alt="">
