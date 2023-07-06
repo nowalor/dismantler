9246561e13821f93acd87d4197a16d5bfa78b190
nowalor <no@walor.io>
2023-07-06 14:04:04 +0200

    progress w/ uploads

--- app/Services/AutoteileMarkDocService.php
+++ app/Services/AutoteileMarkDocService.php
@@ -2,10 +2,92 @@
 
 namespace App\Services;
 
+use App\Models\NewCarPart;
+
 class AutoteileMarkDocService
 {
-    public function generateExportCSV()
-    {
+    private ResolveKbaFromSbrCodeService $resolveKbaFromSbrCodeService;
 
+    public function __construct()
+    {
+        $this->resolveKbaFromSbrCodeService = new ResolveKbaFromSbrCodeService();
+    }
+
+    public function generateExportCSV(NewCarPart $carPart)
+    {
+        $path = base_path('public/exports/import.csv');
+        $file = fopen($path, 'w');
+
+        // Available fields
+        $header = [
+            'supplier_id',
+            'article_nr',
+            'title',
+            'description',
+            'brand',
+            'kba',
+            'part_state',
+            'quantity',
+            'vat',
+            'price',
+            'price_b2b',
+            'img_1',
+            'img_2',
+            'img_3',
+            'img_4',
+            'img_5',
+            'img_6',
+        ];
+
+        fputcsv($file, $header, '|');
+
+        $partInformation = $this->resolvePartInformation($carPart);
+
+        fputcsv($file, $partInformation, '|');
+    }
+
+    private function resolvePartInformation(NewCarPart $carPart): array
+    {
+        $kba = $this->resolveKbaFromSbrCodeService->resolve($carPart->sbr_car_code, $carPart->engine_code);
+        $formattedPart = [
+            'supplier_id' => 'TODO',
+            'article_nr' => $carPart->article_nr,
+            'title' => $carPart->name,
+            'description' => $this->kbaArrayToString($kba),
+            'brand' => $carPart->sbrCode->ditoNumbers->first()->brand,
+            'kba' => $this->kbaArrayToString($kba),
+            'part_state' => '2',
+            'quantity' => '1',
+            'vat' => '19',
+            'price' => $carPart->price * 1.19,
+            'price_b2b' => $carPart->price * 1.19,
+        ];
+
+        $formattedImages = $this->resolveImages($carPart->carPartImages);
+
+        return array_merge($formattedPart, $formattedImages);
+    }
+
+    private function resolveImages($images): array
+    {
+        $formattedImages = [];
+
+        foreach ($images as $index => $image) {
+            $url = asset("storage/img/car-part/{$image->new_car_part_id}/{$image->image_name}");
+
+            $formattedImages["img_$index"] = $url;
+        }
+
+        return $formattedImages;
+    }
+
+    public function resolveDescription(NewCarPart $carPart): string
+    {
+        return '';
+    }
+
+    private function kbaArrayToString(array $kbaArray): string
+    {
+        return implode(',', $kbaArray);
     }
 }
