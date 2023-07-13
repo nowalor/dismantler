9282996ee406292712de296efd98a6b75f7126e1
nowalor <no@walor.io>
2023-07-13 13:50:07 +0200

    see if part is sold on external platform and update in DB

---
+++ app/Console/Commands/ResolvePartsSoldByUsCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Here we check if a part by us is sold on an outside platform
+ * If a part is sold we should reserve it in the data provider
+ * For example if a part we uploaded from the fenix API is sold on autoteile-markt.de
+ * We then need to send a API request to fenix after getting the information from the autoteile-markt FTP
+ * This can work differently depending on the platform and provider
+ */
+namespace App\Console\Commands;
+
+use App\Models\NewCarPart;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class ResolvePartsSoldByUsCommand extends Command
+{
+    protected $signature = 'parts-we-sold:resolve';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        $file = Storage::disk('ftp')->get('sellout_standard-testing.xml');
+
+        $xml = simplexml_load_string($file);
+
+        foreach ($xml->item as $item) {
+            $articleNr = (string) $item->number;
+
+            // Update part in DB
+            $part = NewCarPart::where('article_nr', $articleNr)->first();
+
+            if ($part) {
+                $part->is_live = false;
+                $part->sold_at = now();
+                $part->sold_on_platform = 'autoteile-markt.de';
+
+                $part->save();
+            }
+
+            // Send API request to Data Provider
+
+        }
+
+        return Command::SUCCESS;
+    }
+}
