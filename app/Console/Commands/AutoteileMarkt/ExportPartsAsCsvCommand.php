718249373469fc8790e8a1c9e6616490d0d579b7
nowalor <no@walor.io>
2023-08-11 13:55:26 +0200

    feat: progress export multipe parts with new command

---
+++ app/Console/Commands/AutoteileMarkt/ExportPartsAsCsvCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands\AutoteileMarkt;
+
+use App\Models\NewCarPart;
+use App\Services\AutoteileMarkDocService;
+use Illuminate\Console\Command;
+
+class ExportPartsAsCsvCommand extends Command
+{
+    protected $signature = 'autoteile-markt:export';
+
+    private AutoteileMarkDocService $csvService;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->csvService = new AutoteileMarkDocService();
+    }
+
+    public function handle(): int
+    {
+        $parts = NewCarPart::has('sbrCode.ditoNumbers')
+            ->where('sbr_car_name', 'like', '%audi%')->get();
+
+        foreach ($parts as $part) {
+            logger()->info($part->id);
+
+            $this->csvService->generateExportCSV($part);
+
+            $part->update(['is_live' => true]);
+        }
+
+        return Command::SUCCESS;
+    }
+}
