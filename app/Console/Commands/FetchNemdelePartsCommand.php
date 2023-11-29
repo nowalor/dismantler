0a982ed1299dc0de60a9353cc78537d57e646d58
nowalor <no@walor.io>
2023-11-29 15:26:28 +0100

    progress with formatting part

--- app/Console/Commands/FetchNemdelePartsCommand.php
+++ app/Console/Commands/FetchNemdelePartsCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Enums\DataProviderEnum;
+use App\Models\DanishCarPartType;
 use App\Models\DitoNumber;
 use Illuminate\Console\Command;
 
@@ -27,6 +28,7 @@ class FetchNemdelePartsCommand extends Command
     private function formatPart(array $part): array
     {
         return [
+            'name' => $part['PartName'],
             'data_provider_id' => DataProviderEnum::Nemdele->value,
             'original_number' => $part['OEMNumber1'],
             'dito_number_id' => $this->getDitonumberId($part),
@@ -34,9 +36,12 @@ class FetchNemdelePartsCommand extends Command
             'price_dkk' => $part['Price'],
             'brand' => $part['ManufacturerName'],
             'vin' => $part['StelNr'],
+            'model_year' => $part['Year'],
+            'originally_created_at' => $part['Created'],
+            'car_part_type_id' => $this->getCarPartTypeId($part), // TODO
+
 
             'model' => $part['Model'], // TODO Need to add to DB??
-            'car_part_type_id' => '???' // TODO
         ];
     }
 
@@ -53,5 +58,24 @@ class FetchNemdelePartsCommand extends Command
         }
 
         return $ditoNumber->id;
+    }
+
+    private function getCarPartTypeId(array $part): int | null
+    {
+        $ditoPartCode = $part['PartType'];
+
+        if(!$ditoPartCode) {
+            return null;
+        }
+
+        $danishPartType = DanishCarPartType::where('code', $ditoPartCode)->first();
+
+        if(!$danishPartType) {
+            // TODO some kind of alert
+
+            return null;
+        }
+
+        return $danishPartType->carPartTypes()->first()->id;
     }
 }
