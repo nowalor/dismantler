64031bd61de58d281860d0622d215d3f6e159b8e
nowalor <no@walor.io>
2023-11-30 18:24:29 +0100

    resolve date

--- app/Console/Commands/FetchNemdelePartsCommand.php
+++ app/Console/Commands/FetchNemdelePartsCommand.php
@@ -7,6 +7,7 @@ use App\Models\DanishCarPartType;
 use App\Models\DitoNumber;
 use App\Models\NewCarPart;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 
 class FetchNemdelePartsCommand extends Command
 {
@@ -35,18 +36,26 @@ class FetchNemdelePartsCommand extends Command
             'data_provider_id' => DataProviderEnum::Nemdele->value,
             'original_number' => $part['OEMNumber1'],
             'dito_number_id' => $this->getDitonumberId($part),
-//            'original_id' => $part['StockID'], TODO
+            'original_id' => $part['StockID'],
             'price_dkk' => $part['Price'],
             'brand' => $part['ManufacturerName'],
             'vin' => $part['StelNr'],
             'model_year' => $part['Year'],
-            'originally_created_at' => $part['Created'],
+            'originally_created_at' => $this->formatDate($part['Created']),
             'car_part_type_id' => $this->getCarPartTypeId($part), // TODO
             'mileage_km' => $part['Mileage'],
 
 
             'model' => $part['Model'], // TODO Need to add to DB??
         ];
+    }
+
+    // Take 22-11-2023 13:10:01 and turn into a datetime
+    private function formatDate(string $date): Carbon
+    {
+        $date = str_replace('/', '-', $date);
+
+        return Carbon::createFromFormat('d-m-Y H:i:s', $date);
     }
 
     private function getDitonumberId(array $part): int | null
