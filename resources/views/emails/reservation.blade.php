08db823771b144c76d243be8e5df8a3d1da6b4c3
nowalor <no@walor.io>
2024-01-29 14:37:16 +0100

    some modifications

--- resources/views/emails/reservation.blade.php
+++ resources/views/emails/reservation.blade.php
@@ -1,7 +1,14 @@
 <div>
-    <h3>New reservation from Autoteile</h3>
+    <h3 style="font-size:16px;">Marcus from currus-connect here</h3>
     <p><span style="font-weight: bold;">Article nr: </span> {{ $dismantleId }}</p>
     <p><span style="font-weight: bold;">Id by Fenix: </span> {{ $fenixId }}</p>
 
     <p>Please reserve this as soon as possible and let us know you have reserved it.</p>
+
+    <br />
+    <p>Best regards,</p>
+    <p><span style="font-weight:bold;">Markus Priess</span></p>
+    <p>CEO</p>
+
+    <img style="width:250px;" src="{{ asset('img/new-logo-german.jpg') }}">
 </div>
