55abec8d9009f6c96ea458b2ca200de055edbabf
nowalor <no@walor.io>
2024-02-16 15:42:36 +0100

    test

--- app/Services/EbayApiService.php
+++ app/Services/EbayApiService.php
@@ -50,13 +50,14 @@ class EbayApiService
     public function handlePartUpload(Collection $parts): void
     {
         $formattedParts = (new FormatPartsForXmlAction())->execute($parts);
+        logger($formattedParts);
 
         $xmlName = (new AddProductToXmlFileAction())->execute($formattedParts);
 
-        (new FtpFileUploadAction())->execute(
-            '/store/product',
-            base_path("public/exports/$xmlName"),
-            $xmlName,
-        );
+//        (new FtpFileUploadAction())->execute(
+//            '/store/product',
+//            base_path("public/exports/$xmlName"),
+//            $xmlName,
+//        );
     }
 }
