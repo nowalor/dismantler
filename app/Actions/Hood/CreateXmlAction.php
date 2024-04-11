39f6c3c7cc0e29a30fe0dcd80cd87c9682e5b176
nowalor <no@walor.io>
2024-04-11 14:19:29 +0200

    no message

--- app/Actions/Hood/CreateXmlAction.php
+++ app/Actions/Hood/CreateXmlAction.php
@@ -2,6 +2,8 @@
 
 namespace App\Actions\Hood;
 
+use App\Models\NewCarPart;
+use App\Services\PartInformationService;
 use Illuminate\Database\Eloquent\Collection;
 use SimpleXMLElement;
 
@@ -9,11 +11,14 @@ class CreateXmlAction
 {
     private string $username;
     private string $apiPassword;
+    private PartInformationService $partInformationService;
+
 
     public function __construct()
     {
         $this->username = config('services.hood.username');
         $this->apiPassword = config('services.hood.api_password');
+        $this->partInformationService = new PartInformationService();
     }
 
     public function execute(
@@ -42,14 +47,21 @@ class CreateXmlAction
 
             $item->addChild('itemMode', 'classic');
             $item->addChild('categoryID', '2413');
+            $item->addChild('itemName', $part->new_name);
             $item->addChild('quantity', '1');
             $item->addChild('condition', 'usedGood');
+//            $item->addChild('description', $this->resolveDescription($part));
+
+            $payOptions = $item->addChild('payOptions');
+            $payOptions->addChild('option', 'wireTransfer');
+            $payOptions->addChild('option', 'invoice');
+            $payOptions->addChild('option', 'payPal');
+
 
             $images = $item->addChild('images');
             foreach($part->carPartImages as $image) {
                 $imageXml = $images->addChild('image');
 
-                logger($image);
                 $imageXml->addChild('imageURL' , $image->original_url);
             }
         }
@@ -57,5 +69,46 @@ class CreateXmlAction
 
 
         return $xml->asXML();
+    }
+
+    // Make this reusable instead of copy paste from doc service
+    private function resolveDescription(NewCarPart $carPart): string
+    {
+
+        $kba = $carPart->my_kba->map(function ($kbaNumber) {
+            return [
+                'hsn' => $kbaNumber->hsn,
+                'tsn' => $kbaNumber->tsn,
+            ];
+        })->toArray();
+
+        $kbaString = $this->kbaArrayToString($kba);
+
+        $engineType = $carPart->engine_type ?? '';
+
+        $description = "
+            Einzelne auf den Fotos abgebildeten Anbauteile sind eventuel nicht mit im Lieferumfang enthalten. Mitglieferte Anbauteile sind von der Gewährleistung ausgeschlossen. \n
+            Lagernummer: $carPart->article_nr \n
+            Originale Ersatzteilnummer: $carPart->original_number \n
+            Motor Kennung: $carPart->full_engine_code \n
+            Motortype: $engineType \n
+            Brandstofftype: $carPart->fuel \n
+            Getriebe: {$this->partInformationService->getGearbox($carPart)} \n
+            Laufleistung: $carPart->mileage_km(km) \n
+            Fahrgestellnummer: $carPart->vin \n
+            Baujahr: $carPart->model_year \n
+            Kbas: $kbaString \n
+        ";
+
+        return $description;
+    }
+
+    private function kbaArrayToString(array $kbaArray): string
+    {
+        $propertiesArray = array_map(function ($kbaNumber) {
+            return $kbaNumber['hsn'] . $kbaNumber['tsn'];
+        }, $kbaArray);
+
+        return implode(',', $propertiesArray);
     }
 }
