e7716b9ef05d47850abaafc532a4fae157e10f0a
nowalor <no@walor.io>
2024-04-12 21:19:12 +0200

    ready

--- app/Console/Commands/Fenix/FenixApiFetchPartsCommand.php
+++ app/Console/Commands/Fenix/FenixApiFetchPartsCommand.php
@@ -25,7 +25,8 @@ class FenixApiFetchPartsCommand extends FenixApiBaseCommand
         $this->authenticate();
 
         $dismantleCompanies = [
-            'W',
+            'as',
+            'p',
         ];
 
 
