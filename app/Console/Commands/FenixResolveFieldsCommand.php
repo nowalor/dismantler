cd558ba148dd60995db40e3864f3e4a419092e18
nowalor <no@walor.io>
2024-04-13 13:05:39 +0200

    mofiy command

--- app/Console/Commands/FenixResolveFieldsCommand.php
+++ app/Console/Commands/FenixResolveFieldsCommand.php
@@ -42,7 +42,7 @@ class FenixResolveFieldsCommand extends Command
 //            ->whereNull('car_part_type_id')
 //            ->get();
 
-        $carParts = NewCarPart::where('dismantle_company_name', 'W')->get();
+        $carParts = NewCarPart::whereIn('dismantle_company_name', ['as', 'p'])->get();
 
         foreach($carParts as $carPart) {
             $carPartTypeId = SwedishCarPartType::where('code', $carPart->sbr_part_code)
