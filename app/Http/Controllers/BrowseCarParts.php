46c9af708ded3390fb8a16c540a68c24bb49dbc8
navidmirzad <navi0221@stud.kea.dk>
2024-04-27 14:39:06 +0200

    no message

--- app/Http/Controllers/BrowseCarParts.php
+++ app/Http/Controllers/BrowseCarParts.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class browseCarParts extends Controller
+class BrowseCarParts extends Controller
 {
-    public function browseCarParts() {
-        return view ('browse-car-parts');
-    }
+    //
 }
