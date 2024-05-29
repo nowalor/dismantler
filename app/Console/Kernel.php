ca064ffbdb50b1e388319da08f7c92efc6f0e6f2
nowalor <no@walor.io>
2024-05-29 10:09:30 +0200

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('parts-we-sold:resolve')->everyMinute(); // Check our sales on fenix
         // $schedule->command('ebay:orders')->everyMinute(); // Check our sales on ebay
         $schedule->command('fenix:add-new-parts')->dailyAt('00:00');
-//        $schedule->command('fenix:resolve-images')->everyThirtyMinutes();
+        $schedule->command('fenix:resolve-images')->everyFifteenMinutes();
 //        $schedule->command('fenix:fetch')->dailyAt('00:00');
         $schedule->command('german:parts:seed')->dailyAt('20:05');
 //        $schedule->command('hood:export')->dailyAt('11:05');
