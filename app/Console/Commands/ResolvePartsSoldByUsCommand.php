1e9897de10e50d17c8b47099c3bbbcae8ed2785e
nowalor <no@walor.io>
2024-06-11 12:51:56 +0200

    no message

--- app/Console/Commands/ResolvePartsSoldByUsCommand.php
+++ app/Console/Commands/ResolvePartsSoldByUsCommand.php
@@ -132,6 +132,12 @@ class ResolvePartsSoldByUsCommand extends FenixApiBaseCommand
         foreach ($parts as $part) {
             $dbPart = NewCarPart::where('article_nr', $part['article_nr'])->first();
 
+            if(!$dbPart) {
+                logger("Couldn't find {$part['article_nr']}");
+                
+                continue;
+            }
+
             // Return if part sold_at is not null
             if ($dbPart->sold_at) {
                 continue;
