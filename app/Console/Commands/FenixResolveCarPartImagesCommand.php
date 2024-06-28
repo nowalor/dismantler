daf1f1dc248825309142ea88216931cd41fa6ac3
nowalor <no@walor.io>
2024-06-28 20:39:19 +0200

    no message

--- app/Console/Commands/FenixResolveCarPartImagesCommand.php
+++ app/Console/Commands/FenixResolveCarPartImagesCommand.php
@@ -19,7 +19,7 @@ class FenixResolveCarPartImagesCommand extends Command
 
     public function handle(): int
     {
-        $replacementImagePath = public_path('img/new-logo-german.jpg');
+        $replacementImagePath = public_path('img/new-logo-german.png');
         $replacementImage = Image::make($replacementImagePath);
 
 //        $carParts = NewCarPart::select(["id", "dismantle_company_name"])
@@ -78,7 +78,7 @@ class FenixResolveCarPartImagesCommand extends Command
                     $tempFilePath = tempnam(sys_get_temp_dir(), 'processed_image');
                     file_put_contents($tempFilePath, $stream);
 
-                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/new-logo2", $tempFilePath, $outputName, 'public');
+                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/new-logo3", $tempFilePath, $outputName, 'public');
 
 
 //                    $image->image_name = $outputName;
