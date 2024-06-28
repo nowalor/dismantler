18083390594a16329800c68f8150cc983bd730fe
nowalor <no@walor.io>
2024-06-28 23:00:15 +0200

    no message

--- app/Console/Commands/FenixResolveCarPartImagesCommand.php
+++ app/Console/Commands/FenixResolveCarPartImagesCommand.php
@@ -80,7 +80,7 @@ class FenixResolveCarPartImagesCommand extends Command
                     $tempFilePath = tempnam(sys_get_temp_dir(), 'processed_image');
                     file_put_contents($tempFilePath, $stream);
 
-                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/german-logo", $tempFilePath, $outputName, 'public');
+                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/test123", $tempFilePath, $outputName, 'public');
 
 
 //                    $image->new_logo_german = $outputName;
