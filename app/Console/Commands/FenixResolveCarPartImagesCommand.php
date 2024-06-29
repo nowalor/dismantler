73e749acabc3fc43ebe205be283feeee23671778
nowalor <no@walor.io>
2024-06-29 22:16:04 +0200

    no message

--- app/Console/Commands/FenixResolveCarPartImagesCommand.php
+++ app/Console/Commands/FenixResolveCarPartImagesCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Actions\Images\ReplaceDismantlerLogoAction;
+use App\Models\CarPart;
 use App\Models\NewCarPart;
 use Exception;
 use File;
@@ -22,26 +23,27 @@ class FenixResolveCarPartImagesCommand extends Command
         $replacementImagePath = public_path('img/dismantler/a/logo.png');
         $replacementImage = Image::make($replacementImagePath);
 
-//        $carParts = NewCarPart::select(["id", "dismantle_company_name"])
-//            ->whereHas('carPartImages', function ($query) {
-//                $query->whereNull('new_logo_german');
-//            })
-////            ->with(['carPartImages' => function ($query) {
-////                $query->whereNull('image_name');
-////            }])
-//            ->with('carPartImages')
+        $carParts = NewCarPart::select(["id", "dismantle_company_name"])
+            ->whereHas('carPartImages', function ($query) {
+                $query->whereNull('new_logo_german');
+            })
+//            ->with(['carPartImages' => function ($query) {
+//                $query->whereNull('image_name');
+//            }])
+            ->with('carPartImages')
 //            ->where('dismantle_company_name', 'A')
-//            ->whereNotNull('engine_code')
-//            ->where('engine_code', '!=', '')
-//            ->has('germanDismantlers')
+            ->whereNotNull('engine_code')
+            ->whereIn('external_part_type_id', CarPart::CAR_PART_TYPE_IDS_TO_INCLUDE)
+            ->where('engine_code', '!=', '')
+            ->has('germanDismantlers')
 //            ->where('price_sek', '>', 0)
 //            ->whereNotNull('price_sek')
 //            ->where('price_sek', '!=', '')
-//            ->whereNull('sold_at')
-//            ->take(300)
-//            ->get();
+            ->whereNull('sold_at')
+            ->take(300)
+            ->get();
 
-        $carParts = NewCarPart::where('id', 15674869788)->get();
+//        $carParts = NewCarPart::where('id', 15674869788)->get();
 
         foreach ($carParts as $carPart) {
             foreach ($carPart->carPartImages as $index => $image) {
@@ -81,9 +83,9 @@ class FenixResolveCarPartImagesCommand extends Command
                     $tempFilePath = tempnam(sys_get_temp_dir(), 'processed_image');
                     file_put_contents($tempFilePath, $stream);
 
-//                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/german-logo", $tempFilePath, $outputName, 'public');
-//                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/new-logo", $tempFilePath, $outputName, 'public');
-                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/testing-new-logo3", $tempFilePath, $outputName, 'public');
+                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/german-logo", $tempFilePath, $outputName, 'public');
+                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/new-logo", $tempFilePath, $outputName, 'public');
+//                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/testing-new-logo3", $tempFilePath, $outputName, 'public');
 
 
                     $image->new_logo_german = $outputName;
@@ -109,7 +111,7 @@ class FenixResolveCarPartImagesCommand extends Command
     private function getScalingHeight(string $dismantleCompany): float
     {
 //        $height = 0.29;
-        $height = 0.2;
+        $height = 0.16;
 
 //        $height = 0.27;
 //        $height = 0.31;
