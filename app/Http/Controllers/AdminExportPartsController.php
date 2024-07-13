b6f366e17db5a258a76d3a9bc7eafd7bd1ea6b74
nowalor <no@walor.io>
2024-07-13 18:11:59 +0200

    no message

--- app/Http/Controllers/AdminExportPartsController.php
+++ app/Http/Controllers/AdminExportPartsController.php
@@ -42,7 +42,7 @@ class AdminExportPartsController extends Controller
             ->with('sbrCode.ditoNumbers.germanDismantlers.engineTypes')
             ->with('carPartImages')
 //            ->whereIn('external_dismantle_company_id', [70])
-            ->whereNotIn('external_part_type_id', CarPart::CAR_PART_TYPE_IDS_TO_INCLUDE)
+            ->whereIn('external_part_type_id', CarPart::CAR_PART_TYPE_IDS_TO_INCLUDE)
 //              ->whereNull('external_part_type_id')
             ->orderBy('model_year', 'desc')
 //            ->where('article_nr', 'like', 'al%')
