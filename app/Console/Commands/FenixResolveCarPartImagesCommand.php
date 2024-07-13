9167e6e5a1e76fde513c39481b6923504949bbc0
nowalor <no@walor.io>
2024-07-13 12:48:24 +0200

    no message

--- app/Console/Commands/FenixResolveCarPartImagesCommand.php
+++ app/Console/Commands/FenixResolveCarPartImagesCommand.php
@@ -52,31 +52,37 @@ class FenixResolveCarPartImagesCommand extends Command
                 'name' => 'p',
                 'logoPath' => public_path('img/dismantler/a/logo.png'),
                 'scalingHeight' => '0.18',
+            ],
+
+            'LI' => [
+                'name' => 'li',
+                'logoPath' => public_path('img/dismantler/a/logo.png'),
+                'scalingHeight' => '0.18',
             ]
         ];
 
-        $carParts = NewCarPart::select(["id", "dismantle_company_name"])
-            ->whereHas('carPartImages', function ($query) {
-                $query->whereNull('new_logo_german');
-            })
-            ->with(['carPartImages' => function ($query) {
-                $query->whereNull('new_logo_german');
-            }])
-            ->with('carPartImages')
-//            ->where('dismantle_company_name', 'A')
-//            ->whereNotNull('engine_code')
-//            ->whereIn('external_part_type_id', CarPart::CAR_PART_TYPE_IDS_TO_INCLUDE)
-//            ->where('engine_code', '!=', '')
-//            ->has('germanDismantlers')
-//            ->where('price_sek', '>', 0)
-//            ->whereNotNull('price_sek')
-//            ->where('price_sek', '!=', '')
-//            ->whereNull('sold_at')
-            ->whereIn('dismantle_company_name', ['BO', 'N', 'P', 'AL'])
-            ->take(300)
-            ->get();
+//        $carParts = NewCarPart::select(["id", "dismantle_company_name"])
+//            ->whereHas('carPartImages', function ($query) {
+//                $query->whereNull('new_logo_german');
+//            })
+//            ->with(['carPartImages' => function ($query) {
+//                $query->whereNull('new_logo_german');
+//            }])
+//            ->with('carPartImages')
+////            ->where('dismantle_company_name', 'A')
+////            ->whereNotNull('engine_code')
+////            ->whereIn('external_part_type_id', CarPart::CAR_PART_TYPE_IDS_TO_INCLUDE)
+////            ->where('engine_code', '!=', '')
+////            ->has('germanDismantlers')
+////            ->where('price_sek', '>', 0)
+////            ->whereNotNull('price_sek')
+////            ->where('price_sek', '!=', '')
+////            ->whereNull('sold_at')
+//            ->whereIn('dismantle_company_name', ['BO', 'N', 'P', 'AL'])
+//            ->take(300)
+//            ->get();
 
-//        $carParts = NewCarPart::where('id', 33949)->get();
+        $carParts = NewCarPart::where('id', 32960)->get();
 
         foreach ($carParts as $carPart) {
             $dismantlerCompany =  $dismantlers[$carPart->dismantle_company_name];
@@ -120,15 +126,15 @@ class FenixResolveCarPartImagesCommand extends Command
                     $tempFilePath = tempnam(sys_get_temp_dir(), 'processed_image');
                     file_put_contents($tempFilePath, $stream);
 
-                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/german-logo", $tempFilePath, $outputName, 'public');
-                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/new-logo", $tempFilePath, $outputName, 'public');
-//                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/newsest-testing9", $tempFilePath, $outputName, 'public');
+//                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/german-logo", $tempFilePath, $outputName, 'public');
+//                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/new-logo", $tempFilePath, $outputName, 'public');
+                    Storage::disk('do')->putFileAs("img/car-part/{$image->new_car_part_id}/newsest-testing9", $tempFilePath, $outputName, 'public');
 
 
 
-                    $image->new_logo_german = $outputName;
-                    $image->priority = $carImageNumber;
-                    $image->save();
+//                    $image->new_logo_german = $outputName;
+//                    $image->priority = $carImageNumber;
+//                    $image->save();
 
                     if (file_exists($tempImagePath)) {
                         unlink($tempImagePath);
