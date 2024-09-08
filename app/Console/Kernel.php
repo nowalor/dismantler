b38a6ab2f935f9d28a41b86489a0654992e0dbac
Nikulas Oskarsson <nikulasoskarsson@Nikulass-MacBook-Pro.local>
2024-09-08 16:51:23 +0000

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
 //        $schedule->command('fenix:fetch')->dailyAt('00:00');
 //        $schedule->command('german:parts:seed')->dailyAt('20:05');
        // $schedule->command('hood:export')->dailyAt('13:06');
-         //$schedule->command('remove:sold:parts')->dailyAt('21:51');
+         $schedule->command('remove:sold:parts')->dailyAt('16:52');
     }
 
     /**
