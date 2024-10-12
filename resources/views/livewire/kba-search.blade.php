79e35ae8f6ec50a9f1ae2255a4df739ba8c46a45
navidmirzad <navi0221@stud.kea.dk>
2024-10-12 13:01:54 +0200

    kba now saves part of type.

--- resources/views/livewire/kba-search.blade.php
+++ resources/views/livewire/kba-search.blade.php
@@ -1,24 +1,32 @@
 <form action="{{ route('car-parts.search-by-code') }}">
     <div style="text-align: left;">
+        <!-- HSN Input -->
         <div class="mb-3">
             <label for="hsn" class="form-label">HSN</label>
-            <input type="text" class="form-control" name="hsn" value="{{ old('hsn') }}" wire:model="hsn">
-        </div>
-        <div class="mb-3">
-            <label for="tsn" class="form-label">TSN</label>
-            <input type="text" class="form-control" name="tsn" value="{{ old('tsn') }}" wire:model="tsn">
+            <input type="text" class="form-control" name="hsn" value="{{ old('hsn', $search['hsn'] ?? '') }}" wire:model="hsn">
         </div>
 
+        <!-- TSN Input -->
+        <div class="mb-3">
+            <label for="tsn" class="form-label">TSN</label>
+            <input type="text" class="form-control" name="tsn" value="{{ old('tsn', $search['tsn'] ?? '') }}" wire:model="tsn">
+        </div>
+
+        <!-- Part Type Selection -->
         <div class="mb-3">
             <label for="part-type" class="form-label">{{__('kba-part-type')}}</label>
-            <select name="part-type" class="form-select" id="part-type" wire:model="partType">
+            <select name="type_id" class="form-select" id="part-type" wire:model="partType">
                 <option value="-1" disabled selected>{{__('kba-search-placeholder')}}</option>
                 @foreach($partTypes as $type)
-                    <option value="{{ $type->id }}">{{ $type->name }}</option>
+                    <option value="{{ $type->id }}" 
+                        @if(isset($search['type_id']) && $type->id == $search['type_id']) selected @endif>
+                        {{ $type->name }}
+                    </option>
                 @endforeach
             </select>
         </div>
 
+        <!-- Search Button -->
         @if(isset($partCount) && $partCount !== -1)
             <div style="margin-top: 1rem;">
                 <button class="btn btn-primary w-100 uppercase" @if($partCount === 0) disabled @endif>
