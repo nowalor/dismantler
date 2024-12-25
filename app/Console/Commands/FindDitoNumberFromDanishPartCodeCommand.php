afd862f8c4f86aaf252a5a5b84fbc255e943d37e
nowalor <no@walor.io>
2024-12-25 20:25:20 +0000

    new test sync command

---
+++ app/Console/Commands/FindDitoNumberFromDanishPartCodeCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\DitoNumber;
+use App\Models\NewCarPart;
+use Illuminate\Console\Command;
+
+class FindDitoNumberFromDanishPartCodeCommand extends Command
+{
+    protected $signature = 'car-parts:danish:find-dito-number';
+
+    public function handle(): int
+    {
+        $parts = NewCarPart::where('name', 'DK')
+            ->whereNull('dito_number')
+            ->whereNotNull('danish_item_code')
+            ->take(10000)
+            ->get();
+
+        foreach ($parts as $part) {
+            $ditoNumberCarCode = substr($part->danish_item_code, 0, 4);
+            $ditoNumber = DitoNumber::where('dito_number', $ditoNumberCarCode)->first();
+
+            $part->dito_number = $ditoNumber;
+            $part->save();
+        }
+
+        return Command::SUCCESS;
+    }
+}
