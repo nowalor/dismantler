954c81daa01a98462f4e076e96808ac65e8ed612
nowalor <no@walor.io>
2024-12-29 20:29:43 +0000

    start checking for danish parts again

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
            $schedule->command('fenix:fetch')->dailyAt('19:00'); // Get the parts from fenix
         //$schedule->command('german:parts:seed')->dailyAt('18:59');
           $schedule->command('hood:export')->dailyAt('11:23');
- /*        $schedule->command('remove:sold:parts')->everyFiveMinutes(); */
+         $schedule->command('remove:sold:parts')->everyFifteenMinutes();
         $schedule->command('fenix:resolve-images')->everyTenMinutes();
          //$schedule->command('egluit:purge-parts')->dailyAt('16:08');
     }
