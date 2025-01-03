8fde7ee211ae5b283b03c4e8490333b288e533e1
Nirakash <nira.kash@outlook.com>
2025-01-03 01:30:07 +0100

    dropdown localization translate

--- database/seeders/MainCategorySeeder.php
+++ database/seeders/MainCategorySeeder.php
@@ -4,11 +4,20 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class MainCategorySeeder extends Seeder
 {
     public function run()
     {
+        // Temporarily disable foreign key constraints
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('main_category_car_part_type')->truncate();
+        DB::table('main_categories')->truncate();
+
+        // Re-enable foreign key constraints
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $categories = [
             'Exhaust system',
             'Axle drive',
@@ -34,7 +43,10 @@ class MainCategorySeeder extends Seeder
         ];
 
         foreach ($categories as $category) {
-            DB::table('main_categories')->insert(['name' => $category]);
+            DB::table('main_categories')->insert([
+                'name' => $category,
+                'translation_key' => Str::slug($category, '_'),
+            ]);
         }
     }
 }
