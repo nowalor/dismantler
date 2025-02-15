0a069250fd99b48241c57ba8349c0ba6444ff567
nowalor <no@walor.io>
2025-02-15 13:28:40 +0100

    ready to try command

--- app/Console/Commands/Fenix/RemoveSoldPartsCommand.php
+++ app/Console/Commands/Fenix/RemoveSoldPartsCommand.php
@@ -35,6 +35,8 @@ class RemoveSoldPartsCommand extends Command
         ];
 
         $partIds = $client->getRemovedParts($dismantlers, '2025-02-10T10:41:07.945Z');
+        logger($partIds);
+        die();
 
         $parts = NewCarPart::whereIn('original_id', $partIds)->whereNull('sold_at')->select([
             'id',
