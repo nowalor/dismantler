e6b73f390b3d31e76f2faca96af6b25d9b5f1eb5
nowalor <no@walor.io>
2025-03-13 13:18:34 +0100

    changes to ebay command

--- app/Console/Commands/Ebay/CreateXmlCommand.php
+++ app/Console/Commands/Ebay/CreateXmlCommand.php
@@ -54,7 +54,8 @@ class CreateXmlCommand extends Command
         $originalNumbers = NewCarPart::
         //whereIn('car_part_type_id', [1])
         /*whereIn('car_part_type_id', [1, 2,3,4,5,6, 14])*/
-        whereIn('car_part_type_id', [1, 2,3,4,5,6, 14])
+      /*  whereIn('car_part_type_id', [1, 2,3,4,5,6, 14])*/
+       whereIn('car_part_type_id', [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
     /*    whereIn('car_part_type_id', [2, 3])*/
             ->whereHas('sbrCode', function ($query) {
                 $query->whereNotNull('producer');
