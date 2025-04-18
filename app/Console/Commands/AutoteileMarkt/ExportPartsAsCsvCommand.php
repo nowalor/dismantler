092c1354e9b26ee5a0feba1152f78af3f6db8bc7
nowalor <no@walor.io>
2025-04-18 11:26:09 +0200

    modify auto upload command

--- app/Console/Commands/AutoteileMarkt/ExportPartsAsCsvCommand.php
+++ app/Console/Commands/AutoteileMarkt/ExportPartsAsCsvCommand.php
@@ -33,6 +33,7 @@ class ExportPartsAsCsvCommand extends Command
             ->where('engine_code', '!=', '')
             ->whereNull('sold_at')
             ->whereNotNull('car_part_type_id')
+            ->whereIn('car_part_type_id', [1,2,3,4,5,6,7,8 ,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
             ->where('is_live', false)
             ->where(function ($query) {
                 $query
