da1938b044d54d21d0ed0eb80bf386c78f1f3e31
nowalor <no@walor.io>
2025-05-29 14:14:32 +0200

    testing something

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -7,6 +7,7 @@ use App\Models\CarBrand;
 use App\Models\CarPartType;
 use App\Models\ManufacturerText;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\App;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
@@ -39,7 +40,18 @@ class LandingPageController extends BaseController
         $brands = $this->sharedData['carBrands'];
 
 
-        $mainCategories = MainCategory::withPartsCount()->get();
+        $mainCategories = Cache::remember('main_categories_with_parts_count', 86400, function () {
+            return MainCategory::select('main_categories.*')
+                ->leftJoin('main_category_car_part_type', 'main_categories.id', '=', 'main_category_car_part_type.main_category_id')
+                ->leftJoin('car_part_types', 'main_category_car_part_type.car_part_type_id', '=', 'car_part_types.id')
+                ->leftJoin('new_car_parts', function ($join) {
+                    $join->on('car_part_types.id', '=', 'new_car_parts.car_part_type_id')
+                        ->whereNull('new_car_parts.sold_at');
+                })
+                ->groupBy('main_categories.id')
+                ->selectRaw('count(new_car_parts.id) as new_car_parts_count')
+                ->get();
+        });
 
         // Get the current locale
         $locale = LaravelLocalization::getCurrentLocale();
